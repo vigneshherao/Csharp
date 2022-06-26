939f675598d140e91deec0d719bd367d93268a24
vignesh <vigneshfornavy@gmail.com>
2022-06-26 11:45:09 +0530

    first day

---
+++ MyConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+using MyConsoleApp1;
+
+String car = "i20";
+String company = "Hyundai";
+//Double Mileage = 12.5;
+Console.WriteLine("Enter your driving kilometers:-");
+int kilo = Convert.ToInt32(Console.ReadLine());
+double result = kilo * 12.5;
+Console.WriteLine("Car name---" + car + "\nCompany name---"+ company);
+Console.WriteLine("you will get mileage of: " + result);
---
+++ MyConsoleApp1/Tasks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp1
+{
+    internal class Tasks
+    {
+        public static void New()
+        {
+            String car = "i20";
+            String company = "Hyundai";
+            //Double Mileage = 12.5;
+            Console.WriteLine("Enter your driving kilometers:-");
+            int kilo = Convert.ToInt32(Console.ReadLine());
+            double result = kilo * 12.5;
+            Console.WriteLine("Car name " + car + "Company name" + company);
+            Console.WriteLine("you will get mileage of: "+result);
+
+
+
+        }
+        
+    }
+}
